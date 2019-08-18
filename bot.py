b8de231a6dc2c26cc05f267b3ccce088557766bf
ADmitri <admitri42@gmail.com>
2019-08-18 19:11:59 +0300

    Added showing list of tasks

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import datetime
+from bson.objectid import ObjectId
 from pymongo import MongoClient
 import telebot
 
@@ -20,9 +22,47 @@ def create_or_find(chat_id):
     return user
 
 
-def send_task(chat_id):
-    pass
+def send_task(chat_id, task_id: ObjectId, select_button=True):
+    """
+    Send formatted task to user
+    :param chat_id:
+    :param task_id:
+    :param select_button:
+    :return:
+    """
+    task = db.tasks.find_one({"_id": task_id})
+    if task is None:
+        raise ValueError("Id {} not exists".format(task_id))
 
+    keyboard = None
+    if select_button:
+        keyboard = telebot.types.InlineKeyboardMarkup()
+        callback_button = telebot.types.InlineKeyboardButton(text="select", callback_data=str(task_id))
+        keyboard.add(callback_button)
+
+    bot.send_message(chat_id, "*{}*\n{}".format(task["name"], task["deadline"]),
+                         reply_markup=keyboard, parse_mode="Markdown")
+
+
+def send_active_tasks(chat_id):
+    """
+    Send list of tasks to user
+    :param chat_id:
+    :return:
+    """
+    tasks = db.tasks.find({
+        "$and": [{"deadline": {
+                    "$gte": datetime.datetime.now(),
+                    "$lt": datetime.datetime.now() + datetime.timedelta(0, 3600*8)
+                    }}, {"executor": {"$exists": False}}]
+                })
+    printed=False
+    for task in tasks:
+        printed = True
+        send_task(chat_id, task["_id"], True)
+
+    if not printed:
+        bot.send_message(chat_id, "There is no tasks for you right now")
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -33,8 +73,8 @@ def start(message):
 
     states:
         0 - not busy
-        1 - choosing task
-        2 - busy
+        1 - busy
+        2 - almost done
     """
     create_or_find(message.chat.id)
     bot.send_message(message.chat.id, "Hello! Welcome to AMC Tasks")
@@ -42,19 +82,20 @@ def start(message):
 
 @bot.message_handler(commands=['tasks'])
 def list_of_tasks(message):
+    print("User wants tasks")
     user = create_or_find(message.chat.id)
     if user["state"] == 0:
-        pass
+        send_active_tasks(message.chat.id)
     elif user["state"] == 1:
-        pass
-    elif user["state"] == 2:
-        task = db.tasks.find_one({"chat_id": message.chat.id})
+        task = db.tasks.find_one({"executor": message.chat.id})
         if task is None:
-            db.users.update_one({"chat_id": message.chat.id}, {"$set": {"state": 1}})
-            raise NotImplementedError
-
-
+            db.users.update_one({"chat_id": message.chat.id}, {"$set": {"state": 0}})
+            send_active_tasks(message.chat.id)
+        else:
+            bot.send_message(message.chat.id, "You have active tasks")
+            send_task(message.task.id, task["_id"], False)
 
 
 if __name__ == '__main__':
+    print("Bot started...")
     bot.polling(none_stop=True)
