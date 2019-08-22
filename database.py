b950b2de6cfdec88fad8569f7f46d8944194cf24
ADmitri <admitri42@gmail.com>
2019-08-22 14:13:01 +0300

    End of refactoring

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
 from pymongo import MongoClient
+from datetime import datetime, timedelta
+from bson import ObjectId
 
 NOTBUSY = 0
 BUSY = 1
 ALMOSTDONE = 2
 
+SLOTDURATION = 60*8
 
 class User:
     def __init__(self, chat_id, username, first_name, last_name, state, db):
@@ -50,22 +53,34 @@ class User:
 
 
 class Task:
-    def __init__(self, name, deadline, _id):
+    def __init__(self, name, deadline, done, _id, db, executor=None):
         self.name = name
         self.deadline = deadline
-        self._id = _id
+        self.id = _id
+        self.db = db
+        self.executor = executor
+        self.done=done
 
     @staticmethod
-    def from_dict(task):
-        pass
+    def from_dict(task, db):
+        return Task(**task, db=db)
 
     def __str__(self):
-        return "*{}*\n{}".format()
+        return "*{}*\n{}".format(self.name, self.deadline)
+
+    def add_executor(self, executor: User):
+        self.db.tasks.update_one({"_id": self.id}, {"$set": {"executor": executor.chat_id}})
+        self.executor = executor.chat_id
+
+    def set_done(self):
+        self.db.tasks.update_one({"_id": self.id}, {"$set": {"done": True}})
+        self.done = True
+
 
 class Database:
     def __init__(self, mongo_uri):
         self.client = MongoClient(mongo_uri)
-        self.db = self.client
+        self.db = self.client.AMCtasks
 
     def create_user(self, chat_id, username, first_name, last_name):
         user = {
@@ -90,3 +105,28 @@ class Database:
 
         return user
 
+    def get_task(self, task_id: ObjectId):
+        task = self.db.tasks.find_one({"_id": task_id})
+        if task is None:
+            raise ValueError("Id {} not exists".format(task_id))
+        task = Task.from_dict(task, self.db)
+
+        return task
+
+    def find_task(self, executor: User):
+        task = self.db.tasks.find_one(
+            {"$and": [{"executor": executor.chat_id}, {"done": False}]}, {'_id': 1})
+        if task:
+            return self.get_task(task['_id'])
+        else:
+            return None
+
+    def get_active_tasks_id(self):
+        tasks_id = self.db.tasks.find({
+                        "$and": [{"deadline": {
+                            "$gte": datetime.now(),
+                            "$lt": datetime.now() + timedelta(0, 3600 * 8)
+                        }}, {"executor": {"$exists": False}}]
+                    }, {'_id'}).sort([('deadline', 1)])
+
+        return list(tasks_id)
